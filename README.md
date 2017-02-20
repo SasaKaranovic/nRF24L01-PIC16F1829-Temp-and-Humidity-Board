76b9169eda6897290bdd230ebda5b6f41028dfe0
coldkeyboard <sasa@sasakaranovic.com>
2017-02-20 00:21:34 -0500

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# nRF24L01-PIC16F1829-Temp-and-Humidity-Board
-nRF24L01 and PIC16F1829 (XC8) Temp and Humidity Board Demo
+# nRF24L01 + PIC16F1829 (XC8) + Si7021 (Temp and Humidity) Board Demo
+
+<img src="http://sasakaranovic.com/wp-content/uploads/2016/01/Sensor-Board-Front-1018x1024.jpg" width="400">
+<img src="http://sasakaranovic.com/wp-content/uploads/2016/01/Sensor-Board-Back-1018x1024.jpg" width="400">
+
+### Description: 
+Very simple example of how to use nRF24L01 and PIC16F1829 to send humidity and temperature values from Si7021 sensor over wireless network.
+
+For simplicity this code does address following features: low-power, encryption, handshaking, mesh network, network discovery, network healing. Please approach your IoT application seriously and implement those features properly and responsibly.
+
+If you end up using this code or parts of it, I would love to hear from you and see what did you end up making.
+
+### Links:
+[Sasa Karanovic Home Page](http://sasakaranovic.com "Sasa Karanovic Home Page") - [Project Page](http://sasakaranovic.com/portfolio/new-project-sensor-board-for-smart-home/ "Project page") - [Twitter](https://twitter.com/isalek "Sasa Karanovic @ Twitter")
--- main.c
+++ main.c
@@ -1,7 +1,31 @@
+/*
+
+
+  _________                      ____  __.                                      .__        
+ /   _____/____    ___________  |    |/ _|____ ____________    ____   _______  _|__| ____  
+ \_____  \\__  \  /  ___/\__  \ |      < \__  \\_  __ \__  \  /    \ /  _ \  \/ /  |/ ___\ 
+ /        \/ __ \_\___ \  / __ \|    |  \ / __ \|  | \// __ \|   |  (  <_> )   /|  \  \___ 
+/_______  (____  /____  >(____  /____|__ (____  /__|  (____  /___|  /\____/ \_/ |__|\___  >
+        \/     \/     \/      \/        \/    \/           \/     \/                    \/ 
+
+
+Description: 
+				Very simple example of how to use nRF24L01 and PIC16F1829 to send
+				humidity and temperature values from Si7021 sensor over wireless network.
+				For simplicity this code does address following features: low-power, encryption,
+				handshaking, mesh network, network discovery, network healing. 
+				Please approach your IoT application seriously and implement those features
+				properly and responsibly.
+				
+				If you end up using this code or parts of it, I would love to hear from you
+				and see what did you end up making.
+				
+URL: 			http://sasakaranovic.com/portfolio/new-project-sensor-board-for-smart-home/
+*/
 #include "main.h"
 
 void main(void) {
-    Device_SetUp();
+    Device_SetUp();			
     Device_Initialize();
     Timer1_Setup();
     spi_init();
@@ -12,7 +36,6 @@ void main(void) {
     StatusLED(LED_OFF);
     __delay_ms(500);
     StatusLED(LED_ONLINE);
-    //UART_Debug("Device initialized.\n");
     UART_Debug("----------------\r\n");
     UART_Debug("Sensor Board\r\n");
     UART_Debug("Firmware version: 1.2\r\n");
@@ -20,22 +43,14 @@ void main(void) {
     __delay_ms(500);
     StatusLED(LED_ALL);
     
-    
-    /*
-    wide_band(true), 
-    p_variant(false), 
-    payload_size(32), 
-    ack_payload_available(false), 
-    dynamic_payloads_enabled(false),
-    pipe0_reading_address(0)
-     */
+	//nRF24 Configuration
     wide_band                   = true;
     p_variant                   = false;
     payload_size                = 32;
     ack_payload_available       = true;
     dynamic_payloads_enabled    = true;
     
-    //nRF24
+    //nRF24 Init and Start
     nrf_begin();
     nrf_enableDynamicPayloads();
     nrf_setAutoAck_all(1);
@@ -45,13 +60,12 @@ void main(void) {
     nrf_setRetries(15,15);
     nrf_setCRCLength(RF24_CRC_8);
     
-    //0xf0 f0 f0 f0 e1
-    //uint8_t nRF_pipe_writing[5] = {0xf0, 0xf0, 0xf0, 0xf0, 0xe1};
+    //nRF24 Writting address
     uint8_t nRF_pipe_writing[5] = {0xe1, 0xf0, 0xf0, 0xf0, 0xf0};
-    //0x73 65 72 76 31 LL
-    //uint8_t nRF_pipe_reading[5] = {0x73, 0x65, 0x72, 0x76, 0x31};
+    //nRF24 Reading address
     uint8_t nRF_pipe_reading[5] = {0x31, 0x76, 0x72, 0x65, 0x73};
     
+	//Open read and write pipes
     nrf_openWritingPipe(nRF_pipe_writing);
     nrf_openReadingPipe(1, nRF_pipe_reading);
 
@@ -59,56 +73,60 @@ void main(void) {
     
     i2c_init();
     
-    printf("Printf is working\r\n");
+    UART_Debug("Printf is working\r\n");
 
+	//Send configuration details over UART
+	//Disable when not testing/debugging
     //nrf_printDetails();
     
-    
-//    sb_readTemperature();
-//    sb_readHumidity();
-//  
-//    printf("Temp: %d.%02d\r\nHumid %d.%02d\r\n", si7021_tempNum, si7021_tempDec, si7021_humidNum, si7021_humidDec);
-//    
-//    while(1);
 
     __delay_ms(2000);
     
     
     uint8_t i;
-    
+	
+	
+    //Very simple example
     while(1) {
-        StatusLED(LED_ONLINE);
-        nrf_powerUp();
-        __delay_ms(50);
+        StatusLED(LED_ONLINE);	//Turn on On/Off LED
+        nrf_powerUp();			//Power up nRF (wake up)
+        __delay_ms(50);			//delay so the module has some time to set-up
         
-        sb_readTemperature();
-        sb_readHumidity();
-        //E1:TH:27.00:20.00
-        sprintf(sendPayload, "%02X:TH:%d.%02d:%d.%02d", BOARD_ID, si7021_tempNum, si7021_tempDec, si7021_humidNum, si7021_humidDec);
-        printf("Sending data: %s\r\n", sendPayload);
+        sb_readTemperature();	//Read temperature value from Si7021 sensor
+        sb_readHumidity();		//Read humidity value from Si7021 sensor
+        
+		//Other side is expecting payload formated as follows: 	ID	:	Type	:	Value1	:	Value2
+		//														E1	:	TH		:	27.00	:	20.00
+        sprintf(sendPayload, "%02X:TH:%d.%02d:%d.%02d", BOARD_ID, si7021_tempNum, si7021_tempDec, si7021_humidNum, si7021_humidDec);	//Format string
+        printf("Sending data: %s\r\n", sendPayload);	//Avoid printf! This is just a simple example
 
-        StatusLED(LED_WORKING);
-        // Stop listening and write to radio
-        nrf_stopListening();
+        StatusLED(LED_WORKING);	//Turn on Activity LED
+		
+        
+        nrf_stopListening();	// Stop listening and write to radio
         __delay_ms(10);
         
-        // Send to hub
+		
+        // Try to send data to hub
         if ( nrf_write( sendPayload, 32) ) {
-            UART_Debug("Send successful\n\r");
-            StatusLED(LED_ONLINE);
+            UART_Debug("Send successful\n\r");	//Uart debug message
+            StatusLED(LED_ONLINE);				//Set Activity LED to success
         } 
         else {
-            UART_Debug("Send failed\n\r");
-            StatusLED(LED_FAIL);
+            UART_Debug("Send failed\n\r");		//Uart debug message
+            StatusLED(LED_FAIL);				//Set Activity LED to fail
         }
      
     
         
-        //wait response
-        nrf_startListening();
         
+        nrf_startListening();					//wait response
         __delay_ms(20);
-        while ( nrf_available(nRF_pipe_reading) ) {
+		
+		
+		//Read all available data from reading pipe
+        while ( nrf_available(nRF_pipe_reading) ) 		
+		{
             uint8_t len = nrf_getDynamicPayloadSize();
             nrf_read( receivePayload, len);
 
@@ -116,10 +134,13 @@ void main(void) {
         } // End while
         
         
-        __delay_ms(1000);
-        StatusLED(LED_ONLINE);
+        __delay_ms(1000);						//Delay a bit before going to sleep
+        StatusLED(LED_ONLINE);					//Set device On/Off LED
         
 
+		//Implement your low-power sleep routine if device is battery powered or you want to preserve energy
+		//in this example we will just wait for ~2minutes (1s * 120) and then reread humidity and temperature
+		//and send it back to the hub
         for(i=0; i<120; i++) {
             __delay_ms(1000);
         }
